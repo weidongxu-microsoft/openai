6c909f3ca552b258539cb22bf0ff07e3b7f9a5c2
Weidong Xu <weidxu@microsoft.com>
2023-06-05 14:48:11 +0800

    keep conversation within limit of model tokens

--- src/main/java/io/weidongxu/util/chat/Main.java
+++ src/main/java/io/weidongxu/util/chat/Main.java
@@ -15,18 +15,32 @@ import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.http.HttpRequest;
 import com.azure.core.util.Configuration;
 import com.azure.core.util.Context;
+import com.azure.core.util.logging.ClientLogger;
+import com.knuddels.jtokkit.Encodings;
+import com.knuddels.jtokkit.api.Encoding;
+import com.knuddels.jtokkit.api.ModelType;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
 
+    private static final ClientLogger LOGGER = new ClientLogger(Main.class);
+
+    private static final String MODEL_ID = "gpt-4";
+    private static final int MODEL_TOKEN_LIMIT = 8193;
+    private static final int RESPONSE_TOKEN_LIMIT = 1023;
+
+    private static final Encoding ENCODING = Encodings.newDefaultEncodingRegistry().getEncodingForModel(ModelType.GPT_4);
+
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
 
+        initOpenAIClient();
         updatePrompt();
 
         while (true) {
@@ -69,6 +83,7 @@ public class Main {
 
     private static List<ChatMessage> getConversation(String input) {
         conversation.add(new ChatMessage(ChatRole.USER).setContent(input));
+        trimConversation();
         return conversation;
     }
 
@@ -76,18 +91,57 @@ public class Main {
         conversation.add(new ChatMessage(ChatRole.ASSISTANT).setContent(reply));
     }
 
-    private static OpenAIClient completionsClient;
+    private static void trimConversation() {
+        int remainingTokenCount = MODEL_TOKEN_LIMIT - RESPONSE_TOKEN_LIMIT;
 
-    private static String chatComplete(List<ChatMessage> messages) {
-        if (completionsClient == null) {
-            completionsClient = new OpenAIClientBuilder()
-                    .endpoint(Configuration.getGlobalConfiguration().get("ENDPOINT"))
-                    .credential(new AzureKeyCredential(Configuration.getGlobalConfiguration().get("API_KEY")))
-                    .buildClient();
+        remainingTokenCount -= 100; // there seems certain discrepancy about token count between local and backend
+
+        int systemTokenCount = conversation.stream()
+                .filter(m -> m.getRole() == ChatRole.SYSTEM)
+                .map(m -> ENCODING.countTokens(m.getContent()))
+                .reduce(0, Integer::sum);
+        LOGGER.verbose("system token count: {}", systemTokenCount);
+
+        remainingTokenCount -= systemTokenCount;
+
+        int index = conversation.size() - 1;
+        for (; index > 0; --index) {
+            ChatMessage message = conversation.get(index);
+            if (message.getRole() != ChatRole.SYSTEM) {
+                int tokens = ENCODING.countTokens(message.getContent());
+
+                remainingTokenCount -= tokens;
+                if (remainingTokenCount <= 0) {
+                    break;
+                }
+            }
         }
 
-        String modelId = "gpt-4";
-        ChatCompletions completions = completionsClient.getChatCompletions(modelId,
+        if (index > 0) {
+            LOGGER.info("discard messages: {}", index);
+
+            List<ChatMessage> trimmedConversation = new ArrayList<>();
+            trimmedConversation.add(conversation.get(0));
+            trimmedConversation.addAll(conversation.subList(index + 1, conversation.size()));
+
+            conversation.clear();
+            conversation.addAll(trimmedConversation);
+        } else {
+            LOGGER.verbose("remaining token count: {}", remainingTokenCount);
+        }
+    }
+
+    private static OpenAIClient completionsClient;
+
+    private static void initOpenAIClient() {
+        completionsClient = new OpenAIClientBuilder()
+                .endpoint(Configuration.getGlobalConfiguration().get("ENDPOINT"))
+                .credential(new AzureKeyCredential(Configuration.getGlobalConfiguration().get("API_KEY")))
+                .buildClient();
+    }
+
+    private static String chatComplete(List<ChatMessage> messages) {
+        ChatCompletions completions = completionsClient.getChatCompletions(MODEL_ID,
                 new ChatCompletionsOptions(messages)
                         .setMaxTokens(1024));
         return completions.getChoices().get(0).getMessage().getContent();
